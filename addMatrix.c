0715e02cf4d65373809a5a85768dba09b3f15ebe
Mahendra522 <30647136+Mahendra522@users.noreply.github.com>
2020-09-23 22:27:27 +0530

    Update addMatrix.c

--- addMatrix.c
+++ addMatrix.c
@@ -3,22 +3,22 @@
 int main(void) {
 	int m1[3][3]={0};
 	int m2[3][3]={0};
-  int result[3][3]={0};
+  	int result[3][3]={0};
 	
 	
 	printf("Enter elements of first Matrix:" );
 	for(int i=0; i<3; i++){
-    for(int j=0; j<3;j++){
+    	 for(int j=0; j<3;j++){
 	        scanf("%d",&m1[i][j]);
 	    }
-  }
+  	}
 
-  printf("Enter elements of Second Matrix:" );
+	printf("Enter elements of Second Matrix:" );
 	for(int i=0; i<3; i++){
-    for(int j=0; j<3;j++){
+    	 for(int j=0; j<3;j++){
 	        scanf("%d",&m2[i][j]);
 	    }
-  }
+  	}
 	    
   
 	for(int i=0; i<3; i++){
@@ -27,8 +27,8 @@ int main(void) {
 	    }
 	}
 
-  printf("Addition of 2 Matrix: \n" );
-  for(int i=0; i<3; i++){
+  	printf("Addition of 2 Matrix: \n" );
+  	for(int i=0; i<3; i++){
 	    for(int j=0; j<3;j++){
 	        printf("%d ",result[i][j] );
 	    }
